6fe6e69a7389e3dad949b4f4f0aafd88fbbc3c95
arcpro00 <aedantpt@gmail.com>
2024-10-08 12:37:58 -0700

    Added Subscenes and Arrow Keys Alternative for Player Movement

--- Assets/Scripts/Player Movement.cs
+++ Assets/Scripts/Player Movement.cs
@@ -16,35 +16,35 @@ public class PlayerControls : MonoBehaviour
     void Update()
     {
         // All the movement direction if statements(Adjacently and diagonally)
-        if(Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.UpArrow))
+        if(Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.W))
        {
             Move(new Vector2(-1,1),speed);
        }
-        else if(Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.UpArrow))
+        else if(Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.W))
        {
             Move(new Vector2(1,1),speed);
        }
-        else if(Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.DownArrow))
+        else if(Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.A) && Input.GetKey(KeyCode.S))
        {
             Move(new Vector2(-1,-1),speed);
        }
-        else if(Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.DownArrow))
+        else if(Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.S))
        {
             Move(new Vector2(1,-1),speed);
        }
-        else if(Input.GetKey(KeyCode.RightArrow))
+        else if(Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.D))
        {
             Move(Vector2.right,speed);
        }
-        else if(Input.GetKey(KeyCode.LeftArrow))
+        else if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.A))
        {
             Move(Vector2.left,speed);
        }
-        else if(Input.GetKey(KeyCode.UpArrow))
+        else if(Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W))
        {
             Move(Vector2.up,speed);
        }
-        else if(Input.GetKey(KeyCode.DownArrow))
+        else if(Input.GetKey(KeyCode.DownArrow) || Input.GetKey(KeyCode.S))
        {
             Move(Vector2.down,speed);
        }
