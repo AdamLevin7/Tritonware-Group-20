2d49c9e09263dc6f6ffb0fe7afa75f45db159d67
AdamLevin7 <adamarilevin@gmail.com>
2024-10-13 15:00:27 -0700

    Timer Works betwen scenes. Yay!

--- Assets/Scripts/InventorySystem.cs
+++ Assets/Scripts/InventorySystem.cs
@@ -13,7 +13,7 @@ public class InventorySystem : MonoBehaviour
         if (instance == null)
         {
             instance = this;
-            DontDestroyOnLoad(gameObject); // Keeps this object alive across scenes
+            DontDestroyOnLoad(this.gameObject); // Keeps this object alive across scenes
         }
         else
         {
