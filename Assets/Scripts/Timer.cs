6483e1f2a306588a554c8bb8f90c1b396a5f67d1
AdamLevin7 <adamarilevin@gmail.com>
2024-10-13 14:15:30 -0700

    Timer Works :D

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using UnityEngine.Events;
+
+public class Timer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float startTime = 60.0f;
+    private float time;
+
+    public Image StopWatch;
+
+    public string loseScene;
+
+    void Start (){
+        time = startTime;
+    }
+    
+    // Update is called once per frame
+    void Update()
+    {
+       time -= Time.deltaTime;
+       if(time <= 0.0f){
+        lose();
+       }
+       StopWatch.fillAmount -= 1.0f / startTime * Time.deltaTime;
+    }
+    void lose(){
+        SceneManager.LoadScene(loseScene);
+    }
+}
